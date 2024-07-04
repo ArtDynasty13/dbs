108637bef8abbd0397187b7cf0439a77ee64128c
ArtDynasty13 <arthurwitte18@gmail.com>
2024-07-04 10:48:24 -0600

    move index to static

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
-// Serve static files from the 'static' directory
-app.use('/static', express.static(path.join(__dirname, 'static')));
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
